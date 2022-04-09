05fc2e2cb7e00cb226a00e8923d4efe425bd3af0
Evan Vinciguerra <evanv511@gmail.com>
2022-04-09 18:23:05 -0400

    waffles

--- color.js
+++ color.js
@@ -2,74 +2,29 @@ window.onload = function () {
   const inputElement = document.getElementById('url-input');
 
   inputElement.addEventListener('input', (event) => {
-    const imageElement = document.getElementById('image');
-    imageElement.src = event.target.value;
+    // const imageElement = document.getElementById('image');
+    // imageElement.src = event.target.value;
+
+    // const colorThief = new ColorThief();
+
+    // // Make sure image is finished loading
+    // if (imageElement.complete) {
+    //   colorThief.getColor(imageElement);
+    // } else {
+    //   imageElement.addEventListener('load', function () {
+    //     colorThief.getColor(imageElement);
+    //   });
+    // }
 
     const colorThief = new ColorThief();
+    const img = new Image();
 
-    // Make sure image is finished loading
-    if (imageElement.complete) {
-      colorThief.getColor(imageElement);
-    } else {
-      imageElement.addEventListener('load', function () {
-        colorThief.getColor(imageElement);
-      });
-    }
+    img.addEventListener('load', function () {
+      let x = colorThief.getColor(img);
+      console.log(x);
+    });
 
-    // var img = document.createElement('img');
-    // img.setAttribute('crossOrigin', '');
-    // img.setAttribute('src', event.target.value);
-
-    // var rgb = getAverageRGB(img);
-    // document.body.style.backgroundColor =
-    //   'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
+    img.crossOrigin = 'Anonymous';
+    img.src = event.target.value;
   });
-};
-
-getAverageRGB = (imgEl) => {
-  var blockSize = 5, // only visit every 5 pixels
-    defaultRGB = { r: 0, g: 0, b: 0 }, // for non-supporting envs
-    canvas = document.createElement('canvas'),
-    context = canvas.getContext && canvas.getContext('2d'),
-    data,
-    width,
-    height,
-    i = -4,
-    length,
-    rgb = { r: 0, g: 0, b: 0 },
-    count = 0;
-
-  if (!context) {
-    return defaultRGB;
-  }
-
-  height = canvas.height =
-    imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-  context.drawImage(imgEl, 0, 0);
-
-  try {
-    data = context.getImageData(0, 0, width, height);
-  } catch (e) {
-    console.log(e);
-    /* security error, img on diff domain */ alert('x');
-    return defaultRGB;
-  }
-
-  length = data.data.length;
-
-  while ((i += blockSize * 4) < length) {
-    ++count;
-    rgb.r += data.data[i];
-    rgb.g += data.data[i + 1];
-    rgb.b += data.data[i + 2];
-  }
-
-  // ~~ used to floor values
-  rgb.r = ~~(rgb.r / count);
-  rgb.g = ~~(rgb.g / count);
-  rgb.b = ~~(rgb.b / count);
-
-  return rgb;
 };
