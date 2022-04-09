e4318c0bb4501c20699bbd4f4f152eb883d08506
Evan Vinciguerra <evanv511@gmail.com>
2022-04-09 18:27:43 -0400

    color grabber

--- color.js
+++ color.js
@@ -2,26 +2,16 @@ window.onload = function () {
   const inputElement = document.getElementById('url-input');
 
   inputElement.addEventListener('input', (event) => {
-    // const imageElement = document.getElementById('image');
-    // imageElement.src = event.target.value;
-
-    // const colorThief = new ColorThief();
-
-    // // Make sure image is finished loading
-    // if (imageElement.complete) {
-    //   colorThief.getColor(imageElement);
-    // } else {
-    //   imageElement.addEventListener('load', function () {
-    //     colorThief.getColor(imageElement);
-    //   });
-    // }
+    const imageElement = document.getElementById('image');
+    imageElement.src = event.target.value;
 
     const colorThief = new ColorThief();
     const img = new Image();
 
     img.addEventListener('load', function () {
-      const x = colorThief.getColor(img);
-      console.log(x);
+      const c = colorThief.getColor(img);
+      document.body.style.backgroundColor =
+        'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')';
     });
 
     let googleProxyURL =
